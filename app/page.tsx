bd8be6ab9bdf1786ad764962086d9e847ac667b9
Gianluca Lazzarin <gianluca.lazzarin@alesca.it>
2024-03-07 17:43:05 +0100

    modifiche div

--- app/page.tsx
+++ app/page.tsx
@@ -12,24 +12,24 @@ export default function Home() {
       <div className="flex justify-center space-x-4"> 
         {/* Aggiungi l'attributo href con il link desiderato al tag a */}
         <a href="https://x.com/glzzn">
-          <button className="bg-black w-10 h-10 flex items-center justify-center rounded focus:outline-none">
+          <div className="bg-black w-10 h-10 flex items-center justify-center rounded focus:outline-none">
             <FontAwesomeIcon icon={faXTwitter} className="text-white" style={{ transform: 'scale(0.6)', zIndex: 5  }} />
-          </button>
+          </div>
         </a>
         <a href="https://instagram.com/gianlucalazzarin">
-          <button className="bg-black w-10 h-10 flex items-center justify-center rounded focus:outline-none" style={{  zIndex: -1  }} >
+          <div className="bg-black w-10 h-10 flex items-center justify-center rounded focus:outline-none" style={{  zIndex: -1  }} >
             <FontAwesomeIcon icon={faInstagram} className="text-white" style={{ transform: 'scale(0.6)', zIndex: 5  }} />
-          </button>
+          </div>
         </a>
         <a href="https://www.linkedin.com/in/lazzaringianluca/">
-          <button className="bg-black w-10 h-10 flex items-center justify-center rounded focus:outline-none">
+          <div className="bg-black w-10 h-10 flex items-center justify-center rounded focus:outline-none">
           <FontAwesomeIcon icon={faLinkedin} className="text-white" style={{ transform: 'scale(0.6)', zIndex: 5 }} />
-          </button>
+          </div>
         </a>
         <a href="mailto:io@gianlucalazzar.in">
-          <button className="bg-black w-10 h-10 flex items-center justify-center rounded focus:outline-none">
+          <div className="bg-black w-10 h-10 flex items-center justify-center rounded focus:outline-none">
             <FontAwesomeIcon icon={faEnvelope} className="text-yellow-400" style={{ transform: 'scale(0.6)', zIndex: 5  }} />
-          </button>
+          </div>
         </a>
         </div>
       </div>
