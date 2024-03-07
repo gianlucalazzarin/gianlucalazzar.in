1e2550a87d14a8cc72f625b9bfd6ecaf3711eab1
Gianluca Lazzarin <gianluca.lazzarin@alesca.it>
2024-03-07 17:24:08 +0100

    CON ICONE

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,40 @@
-import Image from 'next/image'
-import bgImage from '../public/backgroundIMG.jpg';
+import bgImage from '../public/background.jpg'; // Assicurati di sostituire con il percorso della tua immagine
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope} from '@fortawesome/free-solid-svg-icons'
+import { faInstagram, faXTwitter, faLinkedin } from '@fortawesome/free-brands-svg-icons'; // Icona brand per Instagram
+
 
 export default function Home() {
+
   return (
-    <div className="flex items-center justify-center h-screen bg-cover bg-center" style={{ backgroundImage: `url(${bgImage.src})` }}>
+    <div className={`flex items-center justify-center h-screen bg-cover bg-center`} style={{ backgroundImage: `url(${bgImage.src})` }}>
       <div className="text-center">
+      <div className="flex justify-center space-x-4"> 
+        {/* Aggiungi l'attributo href con il link desiderato al tag a */}
+        <a href="https://x.com/lazzaringl">
+          <button className="bg-black w-20 h-20 flex items-center justify-center rounded focus:outline-none">
+            <FontAwesomeIcon icon={faXTwitter} className="text-white" style={{ transform: 'scale(0.6)' }} />
+          </button>
+        </a>
+        <a href="https://instagram.com/gianlucalazzarin">
+          <button className="bg-black w-20 h-20 flex items-center justify-center rounded focus:outline-none">
+            <FontAwesomeIcon icon={faInstagram} className="text-white" style={{ transform: 'scale(0.6)' }} />
+          </button>
+        </a>
+        <a href="https://www.linkedin.com/in/lazzaringianluca/">
+          <button className="bg-black w-20 h-20 flex items-center justify-center rounded focus:outline-none">
+            <FontAwesomeIcon icon={faLinkedin} className="text-white" style={{ transform: 'scale(0.6)' }} />
+          </button>
+        </a>
+        <a href="mailto:io@gianlucalazzar.in">
+          <button className="bg-black w-20 h-20 flex items-center justify-center rounded focus:outline-none">
+            <FontAwesomeIcon icon={faEnvelope} className="text-yellow-400" style={{ transform: 'scale(0.6)' }} />
+          </button>
+        </a>
+        </div>
       </div>
     </div>
-  )
+  );
 }
+
+
