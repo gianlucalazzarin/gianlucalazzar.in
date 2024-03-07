28db0adbdb9b4e4ab4f4e706012ea2f1b715f768
Gianluca Lazzarin <gianluca.lazzarin@alesca.it>
2024-03-07 17:46:57 +0100

    new

--- app/page.tsx
+++ app/page.tsx
@@ -12,22 +12,22 @@ export default function Home() {
       <div className="flex justify-center space-x-4"> 
         {/* Aggiungi l'attributo href con il link desiderato al tag a */}
         <a href="https://x.com/glzzn">
-          <div className="bg-black w-10 h-10 flex items-center justify-center rounded focus:outline-none">
+          <div className="w-10 h-10 flex items-center justify-center rounded focus:outline-none">
             <FontAwesomeIcon icon={faXTwitter} className="text-white" style={{ transform: 'scale(0.6)', zIndex: 5  }} />
           </div>
         </a>
         <a href="https://instagram.com/gianlucalazzarin">
-          <div className="bg-black w-10 h-10 flex items-center justify-center rounded focus:outline-none" style={{  zIndex: -1  }} >
+          <div className= "w-10 h-10 flex items-center justify-center rounded focus:outline-none" style={{  zIndex: -1  }} >
             <FontAwesomeIcon icon={faInstagram} className="text-white" style={{ transform: 'scale(0.6)', zIndex: 5  }} />
           </div>
         </a>
         <a href="https://www.linkedin.com/in/lazzaringianluca/">
-          <div className="bg-black w-10 h-10 flex items-center justify-center rounded focus:outline-none">
+          <div className="w-10 h-10 flex items-center justify-center rounded focus:outline-none">
           <FontAwesomeIcon icon={faLinkedin} className="text-white" style={{ transform: 'scale(0.6)', zIndex: 5 }} />
           </div>
         </a>
         <a href="mailto:io@gianlucalazzar.in">
-          <div className="bg-black w-10 h-10 flex items-center justify-center rounded focus:outline-none">
+          <div className="w-10 h-10 flex items-center justify-center rounded focus:outline-none">
             <FontAwesomeIcon icon={faEnvelope} className="text-yellow-400" style={{ transform: 'scale(0.6)', zIndex: 5  }} />
           </div>
         </a>
