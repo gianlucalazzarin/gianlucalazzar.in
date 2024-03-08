1eaaf40408f17cad7d55512bc32cc813938634ff
Gianluca Lazzarin <gianluca.lazzarin@alesca.it>
2024-03-08 12:43:16 +0100

    aggiornamento font awesome

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,29 @@
 import bgImage from '../public/background.jpg';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faInstagram, faXTwitter, faLinkedin } from '@fortawesome/free-brands-svg-icons';
-import { faEnvelope} from '@fortawesome/free-solid-svg-icons'
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 export default function Home() {
   return (
     <div className={`flex items-center justify-center h-screen bg-cover bg-center`} style={{ backgroundImage: `url(${bgImage.src})` }}>
       <div className="text-center">
-        <div className="flex justify-center space-x-4"> 
+        <div className="flex justify-center space-x-4">
           <a href="https://x.com/glzzn">
             <div className="bg-black w-10 h-10 flex items-center justify-center rounded focus:outline-none">
-              <FontAwesomeIcon icon={faXTwitter} className="text-white" style={{ transform: 'scale(0.6)'}} />
+              <i className="fab fa-twitter text-white" style={{ fontSize: '24px' }}></i>
             </div>
           </a>
           <a href="https://instagram.com/gianlucalazzarin">
-            <div className="bg-black w-10 h-10 flex items-center justify-center rounded focus:outline-none" >
-              <FontAwesomeIcon icon={faInstagram} className="text-white" style={{ transform: 'scale(0.6)'  }} />
+            <div className="bg-black w-10 h-10 flex items-center justify-center rounded focus:outline-none">
+              <i className="fab fa-instagram text-white" style={{ fontSize: '24px' }}></i>
             </div>
           </a>
           <a href="https://www.linkedin.com/in/lazzaringianluca/">
             <div className="bg-black w-10 h-10 flex items-center justify-center rounded focus:outline-none">
-              <FontAwesomeIcon icon={faLinkedin} className="text-white" style={{ transform: 'scale(0.6)' }} />
+              <i className="fab fa-linkedin-in text-white" style={{ fontSize: '24px' }}></i>
             </div>
           </a>
           <a href="mailto:io@gianlucalazzar.in">
             <div className="bg-black w-10 h-10 flex items-center justify-center rounded focus:outline-none">
-              <FontAwesomeIcon icon={faEnvelope} className="text-yellow-400" style={{ transform: 'scale(0.6)'  }} />
+              <i className="fas fa-envelope text-yellow-400" style={{ fontSize: '24px' }}></i>
             </div>
           </a>
         </div>
@@ -33,5 +31,3 @@ export default function Home() {
     </div>
   );
 }
-
-
