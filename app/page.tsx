986af9e3b96415594c3fb33044234bd1e9c23722
Gianluca Lazzarin <gianluca.lazzarin@alesca.it>
2024-03-14 12:16:34 +0100

    LINK PHOTO

--- app/page.tsx
+++ app/page.tsx
@@ -4,31 +4,38 @@ import 'animate.css';
 
 export default function Home() {
   return (
-    <div className={`flex items-center justify-center h-screen bg-cover bg-center animate__animated animate__fadeIn`} style={{ backgroundImage: `url(${bgImage.src})` }}>
-      <div className="text-center">
-        <div className="flex justify-center space-x-4 animate__animated animate__fadeInUp animate__delay-2s">
-          <a href="https://x.com/glzzn">
-            <div className="bg-black w-10 h-10 flex items-center justify-center rounded focus:outline-none">
-              <i className="fab fa-x-twitter text-white" style={{ fontSize: '24px' }}></i>
-            </div>
-          </a>
-          <a href="https://instagram.com/gianlucalazzarin">
-            <div className="bg-black w-10 h-10 flex items-center justify-center rounded focus:outline-none">
-              <i className="fab fa-instagram text-white" style={{ fontSize: '24px' }}></i>
-            </div>
-          </a>
-          <a href="https://www.linkedin.com/in/lazzaringianluca/">
-            <div className="bg-black w-10 h-10 flex items-center justify-center rounded focus:outline-none">
-              <i className="fab fa-linkedin-in text-white" style={{ fontSize: '24px' }}></i>
-            </div>
-          </a>
-          <a href="mailto:io@gianlucalazzar.in">
-            <div className="bg-black w-10 h-10 flex items-center justify-center rounded focus:outline-none">
-              <i className="fas fa-envelope text-yellow-400" style={{ fontSize: '24px' }}></i>
-            </div>
-          </a>
+    <div className="h-screen bg-cover bg-center animate__animated animate__fadeIn" style={{ backgroundImage: `url(${bgImage.src})` }}>
+      <div className="flex flex-col justify-center h-full">
+        <div className="text-center">
+          <div className="flex justify-center space-x-4 animate__animated animate__fadeInUp animate__delay-2s">
+            <a href="https://x.com/glzzn">
+              <div className="bg-black w-10 h-10 flex items-center justify-center rounded focus:outline-none">
+                <i className="fab fa-x-twitter text-white" style={{ fontSize: '24px' }}></i>
+              </div>
+            </a>
+            <a href="https://instagram.com/gianlucalazzarin">
+              <div className="bg-black w-10 h-10 flex items-center justify-center rounded focus:outline-none">
+                <i className="fab fa-instagram text-white" style={{ fontSize: '24px' }}></i>
+              </div>
+            </a>
+            <a href="https://www.linkedin.com/in/lazzaringianluca/">
+              <div className="bg-black w-10 h-10 flex items-center justify-center rounded focus:outline-none">
+                <i className="fab fa-linkedin-in text-white" style={{ fontSize: '24px' }}></i>
+              </div>
+            </a>
+            <a href="mailto:io@gianlucalazzar.in">
+              <div className="bg-black w-10 h-10 flex items-center justify-center rounded focus:outline-none">
+                <i className="fas fa-envelope text-yellow-400" style={{ fontSize: '24px' }}></i>
+              </div>
+            </a>
+          </div>
         </div>
       </div>
+      <footer className="text-center p-4 absolute bottom-0 w-full animate__animated animate__fadeInUp animate__delay-3s">
+        <a href="https://photo.gianlucalazzar.in">
+          <span className="text-white" style={{ fontFamily: "Courier New" }}>photo.gianlucalazzar.in</span>
+        </a>
+      </footer>
     </div>
   );
 }
