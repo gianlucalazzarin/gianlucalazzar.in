a7a1dae64bb388fcd46f8f19f691e35c5addafaf
Gianluca Lazzarin <gianluca.lazzarin@gmail.com>
2025-09-15 17:14:26 +0200

    bio3

--- app/page.tsx
+++ app/page.tsx
@@ -55,10 +55,8 @@ export default function Home() {
               </h2>
 
               <p className="text-gray-100/90 mt-3 max-w-md">
-Sono Gianluca Lazzarin, informatico, sviluppatore full stack, grande appassionato e studioso di tecnologia.
-              Credo nell’apprendimento continuo e nell’aggiornamento delle competenze per restare al passo con le più recenti innovazioni.
-              In parallelo al lavoro, studio Filosofia ed Etica dell’Intelligenza Artificiale per comprendere al meglio l’impatto dell’IA sulla mia attività professionale, preparandomi così alle sfide del futuro.
-              Il mio obiettivo è crescere ulteriormente nello sviluppo di soluzioni SaaS, creando prodotti sempre più innovativi.              </p>
+Informatico, sviluppatore full stack, grande appassionato e studioso di tecnologia.
+              </p>
 
               {/* Icone social accessibili */}
               <nav aria-label="Link ai profili social" className="mt-5">
