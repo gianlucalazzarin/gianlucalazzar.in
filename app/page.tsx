c092289c7894c92946910c12c6237e3bc97e2fa6
Gianluca Lazzarin <gianluca.lazzarin@gmail.com>
2025-09-15 12:04:55 +0200

    test

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,42 @@
+// app/page.js (o pages/index.js se usi pages/)
+
 import bgImage from '../public/background.jpg';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'animate.css';
 
 export default function Home() {
   return (
-    <div className="h-screen bg-cover bg-center animate__animated animate__fadeIn" style={{ backgroundImage: `url(${bgImage.src})` }}>
+    <div
+      className="h-screen bg-cover bg-center bg-no-repeat animate__animated animate__fadeIn"
+      style={{
+        backgroundImage: `url(${bgImage.src})`,
+        imageRendering: 'auto',
+        backfaceVisibility: 'hidden',
+        transform: 'translateZ(0)',
+        willChange: 'transform',
+      }}
+    >
       <div className="flex flex-col justify-center h-full">
         <div className="text-center">
           <div className="flex justify-center space-x-4 animate__animated animate__fadeInUp animate__delay-2s">
             <a href="https://x.com/glzzn">
               <div className="bg-black w-10 h-10 flex items-center justify-center rounded focus:outline-none">
-                <i className="fab fa-x-twitter text-white" style={{ fontSize: '24px' }}></i>
+                <i className="fab fa-x-twitter text-white text-2xl"></i>
               </div>
             </a>
             <a href="https://instagram.com/gianlucalazzarin">
               <div className="bg-black w-10 h-10 flex items-center justify-center rounded focus:outline-none">
-                <i className="fab fa-instagram text-white" style={{ fontSize: '24px' }}></i>
+                <i className="fab fa-instagram text-white text-2xl"></i>
               </div>
             </a>
             <a href="https://www.linkedin.com/in/lazzaringianluca/">
               <div className="bg-black w-10 h-10 flex items-center justify-center rounded focus:outline-none">
-                <i className="fab fa-linkedin-in text-white" style={{ fontSize: '24px' }}></i>
+                <i className="fab fa-linkedin-in text-white text-2xl"></i>
               </div>
             </a>
             <a href="mailto:io@gianlucalazzar.in">
               <div className="bg-black w-10 h-10 flex items-center justify-center rounded focus:outline-none">
-                <i className="fas fa-envelope text-yellow-400" style={{ fontSize: '24px' }}></i>
+                <i className="fas fa-envelope text-yellow-400 text-2xl"></i>
               </div>
             </a>
           </div>
